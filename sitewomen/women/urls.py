8db1e0db7466d670be7d4765fc72239030a3746b
Ksenia Mayakovskaya <kseniadmay@gmail.com>
2026-02-05 17:35:58 +0300

    Пройдены уроки по перенаправлениям и функции reverse, шаблоны и функция render, передача переменных в шаблоны

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -5,10 +5,11 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index),  # http://127.0.0.1:8000/
+    path('', views.index, name='home'),  # http://127.0.0.1:8000/
     path('women/', views.index),  # http://127.0.0.1:8000/ + women/
+    path('about/', views.about, name='about'),  # http://127.0.0.1:8000/ + about/
     path('cats/', views.categories),  # http://127.0.0.1:8000/ + cats/
-    path('cats/<int:cat_id>/', views.categories),  # http://127.0.0.1:8000/ + cats/2
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),  # http://127.0.0.1:8000/ + cats/dhm68ud
-    path('archive/<year4:year>/', views.archive)  # http://127.0.0.1:8000/ + archive/1994
+    path('cats/<int:cat_id>/', views.categories, name='cats=id'),  # http://127.0.0.1:8000/ + cats/2
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),  # http://127.0.0.1:8000/ + cats/dhm68ud
+    path('archive/<year4:year>/', views.archive, name='archive')  # http://127.0.0.1:8000/ + archive/1994
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,25 +1,52 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
 
 def index(request):  # HttpRequest
-    return HttpResponse('Страница приложения women.')
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+    data = {'title': 'Главная страница',
+            'menu': menu,
+            'float': 28.56,
+            'lst': [1, 2, 'abc', True],
+            'set': [1, 2, 3, 2, 5],
+            'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+            'obj': MyClass(10, 20)
+            }
+
+    return render(request, 'women/index.html', context=data)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
 
 
 def categories_by_slug(request, cat_slug):
     if request.POST:
         print(request.POST)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
 
 
 def archive(request, year):
     if year > 2026:
-        raise Http404()
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+        uri = reverse('cats', args=('sport',))
+        return HttpResponsePermanentRedirect(uri)
+
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
 
 
 def page_not_found(request, exception):
