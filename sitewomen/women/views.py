cb65da74be4f1ea127fa9fff505268eaafbbeafa
Ksenia Mayakovskaya <kseniadmay@gmail.com>
2026-02-06 15:28:39 +0300

    Пройден урок по тегам шаблонов: теги if и for

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -7,24 +7,19 @@ from django.template.defaultfilters import slugify
 menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
 
-class MyClass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2,  'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3,  'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+]
 
 
 def index(request):  # HttpRequest
-    # t = render_to_string('women/index.html')
-    # return HttpResponse(t)
-    data = {'title': 'главная страница?',
+
+    data = {'title': 'Главная страница',
             'menu': menu,
-            'float': 28.56,
-            'lst': [1, 2, 'abc', True],
-            'set': [1, 2, 3, 2, 5],
-            'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
-            'obj': MyClass(10, 20),
-            'url': slugify('The Main Page')
-            }
+            'posts': data_db
+    }
 
     return render(request, 'women/index.html', context=data)
 
