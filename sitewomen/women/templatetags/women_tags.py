39d0811ef3aadad66fe5fb051efed70c881f2c4e
Ksenia Mayakovskaya <kseniadmay@gmail.com>
2026-02-26 01:31:40 +0300

    Прошла урок Добавление тегов на сайт

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from women.models import Category
+from women.models import Category, TagPost
 
 register = template.Library()
 
@@ -8,3 +8,8 @@ register = template.Library()
 def show_categories(cat_selected=0):
     cats = Category.objects.all()
     return {'cats': cats, 'cat_selected': cat_selected}
+
+
+@register.inclusion_tag('women/list_tags.html')
+def show_all_tags():
+    return {'tags': TagPost.objects.all()}
