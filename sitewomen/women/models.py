f16f312a09c2c45cdfe73f5184cbbc80d46c1cb8
Ksenia Mayakovskaya <kseniadmay@gmail.com>
2026-02-27 03:52:42 +0300

    Прошла урок Класс F, Value и метод annotate()

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -64,6 +64,7 @@ class TagPost(models.Model):
 class Husband(models.Model):
     name = models.CharField(max_length=100, db_index=True)
     age = models.IntegerField(null=True)
+    m_count = models.IntegerField(blank=True, default=0)
 
     def __str__(self):
         return self.name
